17e4981414ea8f155eaea96bf88d390948098383
muhammad <arshad@hexasoft.io>
2022-01-22 20:08:20 +0500

    Initial Commit

---
+++ weather/functions.py
@@ -0,0 +1,161 @@
+# -*- coding: UTF-8 -*-
+# '''=================================================
+# @Project: the_weather
+# @File   : functions
+# @IDE    : PyCharm
+# @Author : Muhammad Arshad
+# @Email  : arshad@hexasoft.io
+# @Time   : 22/01/2022 4:48 PM
+# @Desc   :
+# '''=================================================
+import datetime
+import random
+
+
+def get_current_weather_state(weather_code):
+    if weather_code in [200, 201, 202, 210, 211, 212, 221, 230, 231, 232]:
+        # actual Thunderstorm
+        return 'R'
+    elif weather_code in [300, 301, 302, 310, 311, 312, 313, 314, 321]:
+        # actual Drizzle
+        return 'D'
+    elif weather_code in [500, 501, 502, 503, 504, 511, 520, 521, 522, 531]:
+        # actual rain
+        return 'R'
+    elif weather_code in [600, 601, 602, 611, 612, 613, 615, 616, 620, 621, 622]:
+        # actual snow
+        return 'R'
+    elif weather_code == 800:
+        # actual Clear
+        return 'S'
+    elif weather_code in [801, 802, 803, 804]:
+        # actually Cloudy
+        return 'PC'
+    elif weather_code in [701, 711, 721, 731, 741, 751, 761, 762, 771, 781]:
+        # actually Atmosphere different.
+        return 'C'
+
+
+def get_weather_icon(prediction):
+    if prediction == "R":
+        return 'https://openweathermap.org/img/wn/11d@2x.png'
+    elif prediction == "D":
+        return 'https://openweathermap.org/img/wn/09d@2x.png'
+    elif prediction == "C":
+        return 'https://openweathermap.org/img/wn/02d@2x.png'
+    elif prediction == "PC":
+        return 'https://openweathermap.org/img/wn/04d@2x.png'
+    else:
+        return 'https://openweathermap.org/img/wn/01d@2x.png'
+
+
+def get_next_prediction(current_state):
+    x = random.random()
+    if current_state == "R":
+        if 0 < x < 0.2:
+            return "D"
+        elif 0 < x < 0.3:
+            return "R"
+        elif 0 < x < 0.55:
+            return "C"
+        elif 0 < x < 0.85:
+            return "PC"
+        else:
+            return "S"
+    elif current_state == "C":
+        if 0 < x < 0.15:
+            return "D"
+        elif 0 < x < 0.3:
+            return "R"
+        elif 0 < x < 0.45:
+            return "C"
+        elif 0 < x < 0.75:
+            return "PC"
+        else:
+            return "S"
+    elif current_state == "S":
+        if 0 < x < 0.05:
+            return "D"
+        elif 0 < x < 0.1:
+            return "R"
+        elif 0 < x < 0.2:
+            return "C"
+        elif 0 < x < 0.4:
+            return "PC"
+        else:
+            return "S"
+    elif current_state == "D":
+        if 0 < x < 0.15:
+            return "D"
+        elif 0 < x < 0.45:
+            return "R"
+        elif 0 < x < 0.7:
+            return "C"
+        elif 0 < x < 0.95:
+            return "PC"
+        else:
+            return "S"
+    elif current_state == "PC":
+        if 0 < x < 0.1:
+            return "D"
+        elif 0 < x < 0.2:
+            return "R"
+        elif 0 < x < 0.4:
+            return "C"
+        elif 0 < x < 0.5:
+            return "PC"
+        else:
+            return "S"
+    else:
+        return "Invalid"
+
+
+def get_weather_full_name(state):
+    if state == "R":
+        return "Rainy"
+    elif state == "S":
+        return "Sunny"
+    elif state == "C":
+        return "Cloudy"
+    elif state == "PC":
+        return "Partly Cloudy"
+    elif state == "D":
+        return "Drizzling"
+    else:
+        return "Invalid"
+
+
+def get_next_day_date(current_date):
+    return current_date + datetime.timedelta(days=1)
+
+
+def get_day_name(current_date):
+    return current_date.strftime("%A").upper()
+
+
+def get_seven_days_prediction(current_weather_id):
+    seven_days_predictions = []
+    date_time_now = datetime.datetime.now()
+
+    current_weather_state = get_current_weather_state(current_weather_id)
+    seven_days_predictions.append({
+        'date': date_time_now.date(),
+        'day': get_day_name(date_time_now),
+        'weather_condition': get_weather_full_name(current_weather_state),
+        'weather_condition_short_name': current_weather_state,
+        'icon': get_weather_icon(current_weather_state)
+    })
+
+    for i in range(1, 7):
+        date_time_now += datetime.timedelta(days=1)
+        previous_weather_state = seven_days_predictions[i-1]['weather_condition_short_name']
+        current_weather_state = get_next_prediction(previous_weather_state)
+        seven_days_predictions.append({
+            'date': date_time_now.date(),
+            'day': get_day_name(date_time_now),
+            'weather_condition': get_weather_full_name(current_weather_state),
+            'weather_condition_short_name': current_weather_state,
+            'icon': get_weather_icon(current_weather_state)
+        })
+
+    return seven_days_predictions
--- weather/views.py
+++ weather/views.py
@@ -1,113 +1,31 @@
-import datetime
 import json
 
 import requests
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .models import Weather_csv
+from .functions import get_seven_days_prediction
 
 
 def index(request):
-    data = Weather_csv.objects.all()[:7]
-
     appid = 'bc914f3e0eee264ed492ef9c71dcaf28'
     URL = 'http://api.openweathermap.org/data/2.5/weather'
     PARAMS = {'q': 'peshawar', 'appid': appid, 'units': 'metric'}
-
     r = requests.get(url=URL, params=PARAMS)
     res = r.json()
-
-    description = res['weather'][0]['description']
-    res_icon = res['weather'][0]['icon']
-    temp = res['main']['temp']
-    humidity = res['main']['humidity']
-    res_id = res['id']
-    cloud = res['clouds']['all']
-
-    day = datetime.date.today()
-    data_next_7_days = []
-    today_date_day_name = day.strftime("%A").upper()
-    today_day_short_name = today_date_day_name[:3]
-    print(today_date_day_name)
-    for d in data:
-        # if d.day.upper() != today_date_day_name:
-        if d.description == 'rainy':
-            icon = '<i class="wi wi-rain"></i>'
-        elif d.description == 'drizzle':
-            icon = '<i class="wi wi-raindrops"></i>'
-        elif d.description == 'haze':
-            icon = '<i class="wi wi-day-haze"></i>'
-        elif d.description == 'hot':
-            icon = '<i class="wi wi-hot"></i>'
-        elif d.description == 'sunny':
-            icon = '<i class="wi wi-day-sunny"></i>'
-        elif d.description == 'partially-cloud':
-            icon = '<i class="wi wi-day-cloudy"></i>'
-        elif d.description == 'cloud':
-            icon = '<i class="wi wi-cloudy"></i>'
-        elif d.description == 'storm' or d.description == 'thunderstorm':
-            icon = '<i class="wi wi-storm-showers"></i>'
-        elif d.description == 'smog':
-            icon = '<i class="wi wi-smog"></i>'
-        elif d.description == 'smoke':
-            icon = '<i class="wi wi-smoke"></i>'
-        elif d.description == 'windy' or d.description == 'winds' or d.description == 'wind':
-            icon = '<i class="wi wi-windy"></i>'
-        elif d.description == 'hurricane':
-            icon = '<i class="wi wi-hurricane"></i>'
-        elif d.description == 'snow' or d.description == 'snowing' or d.description == 'snowy':
-            icon = '<i class="wi wi-snow"></i>'
-        elif d.description == 'dust' or d.description == 'dusty':
-            icon = '<i class="wi wi-dust"></i>'
-        elif d.description == 'lightning':
-            icon = '<i class="wi wi-lightning"></i>'
-        elif d.description == 'partly-cloudy':
-            icon = '<i class="wi wi-night-partly-cloudy"></i>'
-        else:
-            icon = '<i class="wi wi-na"></i>'
-        data_next_7_days.append({
-            'number': d.number,
-            'day': d.day.upper()[:3],
-            'description': d.description,
-            'icon': icon,
-            'temperature': d.temperature,
-            'humidity': d.humidity,
-        })
-    found_at_index = 0
-    for i, item in enumerate(data_next_7_days):
-        if item['day'] == today_day_short_name:
-            found_at_index = i
-    data_next_7_days[found_at_index] = {
-        'number': res_id,
-        'day': today_day_short_name,
-        'description': description,
-        'icon': res_icon,
-        'temperature': temp,
-        'humidity': humidity,
-    }
-    print(f'today_day_short_name: {today_day_short_name}')
-    day_order = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
-    i = 0
-    while day_order[i] != today_day_short_name:
-        day_order.append(day_order.pop(day_order.index(day_order[i])))
-
-    sorted_data = sorted(data_next_7_days, key=lambda d: day_order.index(d["day"][:3]))
-
-    return render(request, 'index.html', {'data': sorted_data})
+    if 'weather' in res and len(res['weather']) > 0:
+        res_id = res['weather'][0]['id']
+        predictions = get_seven_days_prediction(res_id)
+        return render(request, 'index.html', {'data': predictions})
+    return render(request, 'index.html', {'data': "FETCHED_CURRENT_STATE_FAILED"})
 
 
 def get_data(request):
-    data = Weather_csv.objects.all()[:7]
-    data_objects = []
-    for d in data:
-        data_objects.append({
-            'id': d.id,
-            'number': d.number,
-            'day': d.day,
-            'description': d.description,
-            'icon': d.icon,
-            'temperature': d.temperature,
-            'humidity': d.humidity
-        })
-    return HttpResponse(content=json.dumps({'data': data_objects}), content_type='application/json')
+    appid = 'bc914f3e0eee264ed492ef9c71dcaf28'
+    URL = 'http://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q': 'peshawar', 'appid': appid, 'units': 'metric'}
+    r = requests.get(url=URL, params=PARAMS)
+    res = r.json()
+    res_id = res['id']
+    predictions = get_seven_days_prediction(res_id)
+    return HttpResponse(content=json.dumps({'data': predictions}), content_type='application/json')
